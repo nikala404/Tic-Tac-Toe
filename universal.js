8e53443ebaf1a41b61aa1ad65dcfbb9393101754
nikala <nikoloz.lursmanashvili.1@btu.edu.ge>
2023-11-11 13:18:41 +0400

    i've fixed textcontent bug

--- universal.js
+++ universal.js
@@ -3,7 +3,7 @@ const xSymbol = document.querySelector('.new-div');
 const oSymbol = document.querySelector('.new-div1');
 const turnContainer = document.querySelector('.turn-div');
 const resetContainer = document.querySelector('.reset-div');
-const resetButto = document.getElementsByTagName('button');
+const resetButton = document.getElementsByTagName('button');
 const box1 = document.querySelector('.boxes1');
 const box2 = document.querySelector('.boxes2');
 const box3 = document.querySelector('.boxes3');
@@ -19,32 +19,33 @@ const oScore = document.querySelector('.scoreO');
 const tie = document.querySelector('.TIES');
 const allBoxes = document.querySelectorAll('.boxes');
 const turnArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 //text changing and computer Logic
 allBox.forEach(function (changeText) {
   changeText.addEventListener('click', function () {
+    // Check if the box has already been selected
+    if (changeText.textContent === 'X' || changeText.textContent === 'O') {
+      return;
+    }
+
     let cpuTurn = Math.round(Math.random(turnArray) * turnArray.length);
     const boxNumber =
       Array.from(document.querySelectorAll('.boxes')).indexOf(changeText) + 1;
     if (turnArray.find(num => num === boxNumber)) {
       turnArray.splice(turnArray.indexOf(boxNumber), 1);
-      // console.log(turnArray);
     }
     if (turnArray.includes(cpuTurn)) {
       allBox[cpuTurn - 1].textContent = 'O';
       allBox[cpuTurn - 1].style.color = '#f2b237';
       turnArray.splice(turnArray.indexOf(cpuTurn), 1);
-      // console.log(turnArray);
     } else if (turnArray.includes(cpuTurn) === false) {
       let random = 0;
       while (random < 10) {
         random += 1;
-        // console.log(random);
         if (turnArray.includes(random) === true) {
           allBox[random - 1].textContent = 'O';
           allBox[random - 1].style.color = '#f2b237';
           turnArray.splice(turnArray.indexOf(random), 1);
-          // console.log(random);
-          console.log(turnArray);
           break;
         }
       }
@@ -59,13 +60,13 @@ allBox.forEach(function (changeText) {
     } else {
       changeText.textContent = 'X';
     }
-    console.log(turnArray);
     if (turnArray.length === 0) {
       alert('Tie');
       window.location.reload();
     }
   });
 });
+
 //reset
 resetContainer.addEventListener('click', function () {
   window.location.reload();
